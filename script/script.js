7aa5c6f2a22801715813e305d4a824377125b6aa
Vladislav Tyukachev <vlad.tyukachev1@mail.ru>
2020-06-09 04:49:12 +0300

    Задание к уроку 2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
-let money = 1000, income = 5000, addExpenses = 400, deposit = 25, mission = 'Бюджет', period = '12';
+let money = 30000, income = 'Фриланс', addExpenses = 'Интернет, коммуналка, доставка', deposit = true, mission = 1000000, period = '6';
 
-alert('Экономика');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log('Бюджет 50000');
+console.log(addExpenses.length);
 
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay : ', budgetDay);
