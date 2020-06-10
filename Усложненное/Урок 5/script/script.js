726072065bf119e05bf44677c13f239ce9f97ca6
Vladislav Tyukachev <vlad.tyukachev1@mail.ru>
2020-06-10 10:59:53 +0300

    Усложненное задание к уроку 2

---
+++ Усложненное/Урок 5/script/script.js
@@ -0,0 +1,23 @@
+// задание 1
+
+let arr = ['234', '544', '333', '555', '423', '242', '444', '555'];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] == 2 || arr[i][0] == 4) {
+        console.log(arr[i]);
+    }
+}
+
+// задание 2
+
+let num = 100;
+
+outer:
+for (let i = 2; i < 100; i++) {
+    for (let j = 2; j < i; j++) {
+       if (i % j == 0) {
+        continue outer;
+       }
+    }
+    console.log(i);
+}
